715beae0da4de290dc187641a85ee7610dc7c98f
Chayathon <chayathon_p@cmu.ac.th>
2023-01-13 17:22:37 +0700

    lab14_1 COMPLETE

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,57 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+void stat(const double a[],int n, double b[])
+{
+    double sum=0;
+    for(int i=0 ;i<n;i++)
+    {
+            
+            sum=sum+a[i];
+            b[0]=sum/n;
+    }
+
+
+    double sd=0;
+    for(int i=0  ;i<n;i++)
+    { 
+            sd +=pow((a[i]-b[0]),2);
+    }   
+    b[1]=sqrt(sd/n);
+    
+
+
+    double  g=1;
+    for(int i=0 ;i< n;i++)
+    {
+            g=g*a[i];
+    }
+    b[2]=pow(g,1.0/n);
+
+
+    double sum3=0;
+    for(int i=0 ;i<n;i++)
+    {
+            sum3=sum3+(1.0/a[i]);
+    }
+     b[3]=n/(sum3);
+
+    double max=a[0];
+    for(int i=1 ;i<n;i++)
+    {
+            if(a[i]>max)
+            max=a[i];
+    }
+    b[4]=max;
+
+
+
+    double min=a[0];
+    for(int i=1 ;i<n;i++)
+    {
+            if(a[i]<min)
+            min=a[i];
+    }
+    b[5]=min;
+
+}
